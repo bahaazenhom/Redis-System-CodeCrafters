9397dbee2f1477ef3f6bd1d5da5b510a041c6274
Bahaa Zenhom <bahaazenhom7@gmail.com>
2025-10-30 22:32:43 +0300

    testing

--- src/main/java/replication/ReplicationManager.java
+++ src/main/java/replication/ReplicationManager.java
@@ -235,7 +235,7 @@ public class ReplicationManager {
                 ackCommand.add("*");
                 slaveMasterConnection.write(RESPSerializer.array(ackCommand));
                 slaveMasterConnection.flush();
-                System.out.println("Master asked slave for ACK offset " + ackCommand);
+                System.out.println("Master asked slave for ACK offset " + RESPSerializer.array(ackCommand));
                 // Wait for the acknowledgment from the slave
                // handleAcksCommandsReceivedFromSlaves(slaveMasterConnection);
             } catch (Exception e) {
